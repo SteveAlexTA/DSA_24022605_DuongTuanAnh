3a9fdfb62e02f0bd940d73c0b1f83fabfc63ae10
SteveAlexTA <tuananhahihi2006@gmail.com>
2025-11-10 13:09:41 +0700

    Hash Chaining

--- Week5/HashChaining.cpp
+++ Week5/HashChaining.cpp
@@ -48,20 +48,57 @@ public:
 
     void remove(int key) { // O(n)
         int index = hashFunc(key);
-        Node* cur = htable[index];
+        Node* curr = htable[index];
         Node* prev = nullptr;
-        while (cur != nullptr) {
-            if (cur->key == key) {
-                if (prev == nullptr) {
-                    htable[index] = cur->next;
-                } else {
-                    prev->next = cur->next;
-                }
-                delete cur;
-                return;
-            }
+        while (curr && curr->key != key) { 
+            prev = curr;
+            curr = curr->next;
         }
-        prev = cur;
-        cur = cur->next;
+        if (curr == nullptr) {
+            return;
+        }
+        if (prev == nullptr) { 
+            htable[index] = curr->next;
+        } else {
+            prev->next = curr->next;
+        }
+        delete curr;
     }
+
+    void print() {
+        for (int i = 0; i < size; i++) {
+            cout << i << ": ";
+            Node* curr = htable[i];
+            while (curr) {
+                cout << curr->key << " -> ";
+                curr = curr->next;
+            }
+            cout << "NULL" << endl;
+        }
+    }
+};
+
+int main() {
+    HashTable ht(5); 
+    ht.insert(1);
+    ht.insert(8);
+    ht.insert(13);
+    ht.insert(29);
+
+    cout << "Initial Hash Table:" << endl;
+    ht.print();
+
+    cout << "\nRemove 8:" << endl;
+    ht.remove(8);
+    ht.print();
+
+    cout << "\nRemove 100:" << endl;
+    ht.remove(100);
+    ht.print();
+
+    cout << "\nRemove 1:" << endl;
+    ht.remove(1);
+    ht.print();
+
+    return 0;
 }
